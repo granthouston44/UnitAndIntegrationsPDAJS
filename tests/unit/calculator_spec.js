9e8aa678e6a64939c0bb8f2f6e2174f06ae1bb12
Grant Houston <grant.houston44@hotmail.com>
2020-02-16 09:30:25 +0000

    add test passed

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,21 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+  it('it can add ', function() {
+    calculator.numberClick(1);
+    calculator.operatorClick('+');
+    calculator.numberClick(4);
+    calculator.operatorClick('=');
+    assert.strictEqual(calculator.previousTotal, 5)
+  })
+
+});
