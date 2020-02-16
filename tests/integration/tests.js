1efda1c033ba0b190fe3403ef8d119fa1666094e
Grant Houston <grant.houston44@hotmail.com>
2020-02-16 09:52:55 +0000

    arithmetical operations should update the display with the result of the operation

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -15,6 +15,14 @@ describe('calculator functionality', function() {
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
-  
+  it('arithmetical operations should update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add.operator')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals.operator')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
 
 });
